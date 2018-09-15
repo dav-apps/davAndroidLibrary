817ed89301d933329ced1011b001ed30a1a2c642
David <dav2070@web.de>
2018-09-15 22:44:17 +0200

    Add dataPath, return the ids when inserting into the db, add GlobalScope to launch and async calls and add getTableFolder()

--- src/main/java/app/dav/davandroidlibrary/Dav.kt
+++ src/main/java/app/dav/davandroidlibrary/Dav.kt
@@ -3,8 +3,10 @@ package app.dav.davandroidlibrary
 import android.content.Context
 import app.dav.davandroidlibrary.data.*
 import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.GlobalScope
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
+import java.io.File
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -12,20 +14,22 @@ object Dav {
     const val apiBaseUrl = "https://dav-backend.herokuapp.com/v1/"
     const val DATABASE_NAME = "dav.db"
     var database: DavDatabase? = null
+    var dataPath: String = ""
 
-    fun init(context: Context){
+    fun init(context: Context, dataPath: String){
         database = DavDatabase.getInstance(context)
+        this.dataPath = dataPath
     }
 
     object Database{
-        fun createTableObject(tableObject: TableObject){
-            launch {
-                database?.tableObjectDao()?.insertTableObject(TableObject.convertTableObjectToTableObjectEntity(tableObject))
+        fun createTableObject(tableObject: TableObject) : Deferred<Long>{
+            return GlobalScope.async {
+                database?.tableObjectDao()?.insertTableObject(TableObject.convertTableObjectToTableObjectEntity(tableObject)) ?: 0
             }
         }
 
-        fun createTableObjectWithProperties(tableObject: TableObject){
-            launch {
+        fun createTableObjectWithProperties(tableObject: TableObject) : Deferred<Long>{
+            return GlobalScope.async {
                 val tableObjectEntity = TableObjectEntity(tableObject.tableId, tableObject.uuid.toString(), 0, 0, tableObject.isFile, tableObject.etag)
                 val id = database?.tableObjectDao()?.insertTableObject(tableObjectEntity) ?: 0
                 if(!id.equals(0)){
@@ -34,18 +38,19 @@ object Dav {
                         property.id = database?.propertyDao()?.insertProperty(Property.convertPropertyToPropertyEntity(property)) ?: 0
                     }
                 }
+                id
             }
         }
 
         fun getTableObject(uuid: UUID) : Deferred<TableObject?>{
-            return async {
+            return GlobalScope.async {
                 val tableObjectEntity = database?.tableObjectDao()?.getTableObject(uuid.toString())
                 if(tableObjectEntity != null) TableObject.convertTableObjectEntityToTableObject(tableObjectEntity) else null
             }
         }
 
         fun getAllTableObjects(tableId: Int, deleted: Boolean) : Deferred<ArrayList<TableObject>>{
-            return async {
+            return GlobalScope.async {
                 val db = database
                 val tableObjectEntities = if(db != null) db.tableObjectDao().getNonObservableTableObjects() else listOf()
                 val tableObjects = ArrayList<TableObject>()
@@ -61,25 +66,25 @@ object Dav {
         }
 
         fun updateTableObject(tableObject: TableObject){
-            launch {
+            GlobalScope.launch {
                 database?.tableObjectDao()?.updateTableObject(TableObject.convertTableObjectToTableObjectEntity(tableObject))
             }
         }
 
         fun tableObjectExists(uuid: UUID) : Deferred<Boolean>{
-            return async {
+            return GlobalScope.async {
                 database?.tableObjectDao()?.getTableObject(uuid.toString()) != null
             }
         }
 
         fun createProperty(property: Property){
-            launch {
+            GlobalScope.launch {
                 database?.propertyDao()?.insertProperty(Property.convertPropertyToPropertyEntity(property))
             }
         }
 
         fun getPropertiesOfTableObject(tableObjectId: Long) : Deferred<ArrayList<Property>>{
-            return async {
+            return GlobalScope.async {
                 val db = database
                 val propertyEntries: List<PropertyEntity> = if(db != null) db.propertyDao().getPropertiesOfTableObject(tableObjectId) else listOf<PropertyEntity>()
                 val properties = ArrayList<Property>()
@@ -94,15 +99,25 @@ object Dav {
         }
 
         fun updateProperty(property: Property){
-            launch {
+            GlobalScope.launch {
                 database?.propertyDao()?.updateProperty(Property.convertPropertyToPropertyEntity(property))
             }
         }
 
         fun propertyExists(id: Long) : Deferred<Boolean>{
-            return async {
+            return GlobalScope.async {
                 database?.propertyDao()?.getProperty(id) != null
             }
+        }
+
+        fun getTableFolder(tableId: Int) : File{
+            val path = dataPath + tableId.toString()
+            val file = File(path)
+            if(!file.exists()){
+                // Create the directory
+                file.mkdir()
+            }
+            return file
         }
     }
 }
