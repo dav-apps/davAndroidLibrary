3ea0fad15c88227b7f4d34b10ad3f947c47e74c5
David <dav2070@web.de>
2018-11-22 22:54:48 +0100

    Add IGeneralMethods interface
    Add isNetworkAvailable and getEnvironment function

--- src/main/java/app/dav/davandroidlibrary/common/IGeneralMethods.kt
+++ src/main/java/app/dav/davandroidlibrary/common/IGeneralMethods.kt
@@ -1,4 +1,8 @@
 package app.dav.davandroidlibrary.common
 
+import app.dav.davandroidlibrary.DavEnvironment
+
 interface IGeneralMethods {
+    fun isNetworkAvailable() : Boolean
+    fun getEnvironment() : DavEnvironment
 }
--- src/main/java/app/dav/davandroidlibrary/common/ProjectInterface.kt
+++ src/main/java/app/dav/davandroidlibrary/common/ProjectInterface.kt
@@ -2,4 +2,5 @@ package app.dav.davandroidlibrary.common
 
 object ProjectInterface {
     var localDataSettings: ILocalDataSettings? = null
+    var generalMethods: IGeneralMethods? = null
 }
