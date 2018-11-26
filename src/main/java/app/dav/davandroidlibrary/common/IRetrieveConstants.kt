5814cd970cd9cee303d141d447ff358e5a1e2f96
David <dav2070@web.de>
2018-11-26 22:51:55 +0100

    Add IRetrieveConstants
    Add getDataPath, getApiKey, getAppId and getTableIds functions

---
+++ src/main/java/app/dav/davandroidlibrary/common/IRetrieveConstants.kt
@@ -0,0 +1,8 @@
+package app.dav.davandroidlibrary.common
+
+interface IRetrieveConstants {
+    fun getDataPath() : String
+    fun getApiKey() : String
+    fun getAppId() : Int
+    fun getTableIds() : ArrayList<Int>
+}
--- src/main/java/app/dav/davandroidlibrary/common/ProjectInterface.kt
+++ src/main/java/app/dav/davandroidlibrary/common/ProjectInterface.kt
@@ -3,4 +3,5 @@ package app.dav.davandroidlibrary.common
 object ProjectInterface {
     var localDataSettings: ILocalDataSettings? = null
     var generalMethods: IGeneralMethods? = null
+    var retrieveConstants: IRetrieveConstants? = null
 }
