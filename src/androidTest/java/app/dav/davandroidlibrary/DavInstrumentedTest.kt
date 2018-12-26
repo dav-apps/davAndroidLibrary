68bc66c23f866d3887e6dbab73c721771f436d2f
David <dav2070@web.de>
2018-12-26 16:53:17 +0100

    Write tests for Dav.Database.getTableObject

--- src/androidTest/java/app/dav/davandroidlibrary/DavInstrumentedTest.kt
+++ src/androidTest/java/app/dav/davandroidlibrary/DavInstrumentedTest.kt
@@ -2,6 +2,7 @@ package app.dav.davandroidlibrary
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import android.util.Log
 import app.dav.davandroidlibrary.data.DavDatabase
 import app.dav.davandroidlibrary.models.Property
 import app.dav.davandroidlibrary.models.TableObject
@@ -16,28 +17,30 @@ class DavInstrumentedTest{
     val context = InstrumentationRegistry.getTargetContext()
     val database = DavDatabase.getInstance(context)
 
+    init {
+        Dav.init(context)
+    }
+
     // createTableObject tests
     @Test
-    fun createTableObjectAsyncShouldSaveTheTableObjectInTheDatabaseAndReturnTheId(){
+    fun createTableObjectShouldSaveTheTableObjectInTheDatabaseAndReturnTheId(){
         // Arrange
-        Dav.init(context)
-
         val uuid = UUID.randomUUID()
         val tableId = 4
         val tableObject = TableObject(uuid, tableId)
 
         // Act
-        val id = runBlocking {
-            Dav.Database.createTableObjectAsync(tableObject).await()
+        tableObject.id = runBlocking {
+            Dav.Database.createTableObject(tableObject)
         }
 
         // Assert
-        Assert.assertNotEquals(0, id)
+        Assert.assertNotEquals(0, tableObject.id)
 
         // Get the table object from the database
-        val tableObjectFromDatabase = database.tableObjectDao().getTableObject(id)
+        val tableObjectFromDatabase = database.tableObjectDao().getTableObject(tableObject.id)
         Assert.assertEquals(tableId, tableObjectFromDatabase.tableId)
-        Assert.assertEquals(id, tableObjectFromDatabase.id)
+        Assert.assertEquals(tableObject.id, tableObjectFromDatabase.id)
         Assert.assertEquals(uuid, UUID.fromString(tableObjectFromDatabase.uuid))
         Assert.assertFalse(tableObjectFromDatabase.isFile)
     }
@@ -45,10 +48,8 @@ class DavInstrumentedTest{
 
     // CreateTableObjectWithProperties tests
     @Test
-    fun CreateTableObjectWithPropertiesShouldSaveTheTableObjectAndItsPropertiesInTheDatabase(){
+    fun createTableObjectWithPropertiesShouldSaveTheTableObjectAndItsPropertiesInTheDatabase(){
         // Arrange
-        Dav.init(context)
-
         val uuid = UUID.randomUUID()
         val tableId = 4
         val firstPropertyName = "page1"
@@ -62,14 +63,14 @@ class DavInstrumentedTest{
         val tableObject = TableObject(uuid, tableId, propertiesArray)
 
         // Act
-        val id = runBlocking {
-            Dav.Database.createTableObjectWithPropertiesAsync(tableObject).await()
+        tableObject.id = runBlocking {
+            Dav.Database.createTableObjectWithProperties(tableObject)
         }
 
         // Assert
-        val tableObjectFromDatabase = database.tableObjectDao().getTableObject(id)
+        val tableObjectFromDatabase = database.tableObjectDao().getTableObject(tableObject.id)
         Assert.assertEquals(tableId, tableObjectFromDatabase.tableId)
-        Assert.assertEquals(id, tableObjectFromDatabase.id)
+        Assert.assertEquals(tableObject.id, tableObjectFromDatabase.id)
         Assert.assertEquals(uuid, UUID.fromString(tableObjectFromDatabase.uuid))
 
         val firstPropertyFromDatabase = database.propertyDao().getProperty(tableObject.properties[0].id)
@@ -83,4 +84,43 @@ class DavInstrumentedTest{
         Assert.assertEquals(secondPropertyValue, secondPropertyFromDatabase.value)
     }
     // End createTableObjectWithProperties tests
+
+    // GetTableObject tests
+    @Test
+    fun getTableObjectShouldReturnTheTableObject(){
+        // Arrange
+        val uuid = UUID.randomUUID()
+        val tableId = 4
+        val tableObject = TableObject(uuid, tableId)
+        tableObject.id = runBlocking {
+            Dav.Database.createTableObject(tableObject)
+        }
+
+        // Act
+        val tableObjectFromDatabase = runBlocking {
+             Dav.Database.getTableObject(uuid)
+        }
+
+        // Assert
+        Assert.assertNotNull(tableObjectFromDatabase)
+        Assert.assertEquals(tableObject.id, tableObjectFromDatabase?.id)
+        Assert.assertEquals(tableObject.tableId, tableObjectFromDatabase?.tableId)
+        Assert.assertEquals(tableObject.uuid, tableObjectFromDatabase?.uuid)
+        Assert.assertEquals(tableObject.uploadStatus, tableObjectFromDatabase?.uploadStatus)
+    }
+
+    @Test
+    fun getTableObjectShouldReturnNullWhenTheTableObjectDoesNotExist(){
+        // Arrange
+        val uuid = UUID.randomUUID()
+
+        // Act
+        val tableObject = runBlocking {
+            Dav.Database.getTableObject(uuid)
+        }
+
+        // Assert
+        Assert.assertNull(tableObject)
+    }
+    // End getTableObject tests
 }
