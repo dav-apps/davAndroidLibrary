734236d48e9acf4c03278cbf63fc33687ebcc7a2
David <dav2070@web.de>
2018-12-27 16:52:00 +0100

    Add Constants object with some values

---
+++ src/androidTest/java/app/dav/davandroidlibrary/Constants.kt
@@ -0,0 +1,16 @@
+package app.dav.davandroidlibrary
+
+import app.dav.davandroidlibrary.models.DavPlan
+
+object Constants {
+    const val apiKey = "MhKSDyedSw8WXfLk2hkXzmElsiVStD7C8JU3KNGp"
+    const val appId = 3
+    const val testDataTableId = 3
+    const val testFileTableId = 4
+    const val davDataPath = "/data/user/0/app.dav.davandroidlibrary.test/dav/"
+
+    const val jwt = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmNsYXNzbGlicmFyeXRlc3RAZGF2LWFwcHMudGVjaCIsInVzZXJuYW1lIjoiZGF2Q2xhc3NMaWJyYXJ5VGVzdFVzZXIiLCJ1c2VyX2lkIjoxMiwiZGV2X2lkIjoyLCJleHAiOjM3NTI5MTgzODQxfQ.lO-iq5UHk25wnysbrEw1PirgGBhz-rFqrK6iRGkcFnU"
+    const val testuserEmail = "davclasslibrarytest@dav-apps.tech"
+    const val testuserUsername = "davClassLibraryTestUser"
+    val testuserPlan = DavPlan.Free
+}
