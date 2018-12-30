f4e89da8791de80842b4a05556448bbb9d567d9d
David <dav2070@web.de>
2018-12-30 16:27:58 +0100

    Write tests for TableObject.delete

--- src/androidTest/java/app/dav/davandroidlibrary/models/TableObjectInstrumentedTest.kt
+++ src/androidTest/java/app/dav/davandroidlibrary/models/TableObjectInstrumentedTest.kt
@@ -2,6 +2,7 @@ package app.dav.davandroidlibrary.models
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import app.dav.davandroidlibrary.Constants
 import app.dav.davandroidlibrary.Dav
 import app.dav.davandroidlibrary.common.*
 import app.dav.davandroidlibrary.data.DavDatabase
@@ -9,6 +10,8 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.File
+import java.io.InputStream
 import java.util.*
 
 @RunWith(AndroidJUnit4::class)
@@ -95,7 +98,7 @@ class TableObjectInstrumentedTest {
         val uuid = UUID.randomUUID()
         val tableId = 7
         val propertyName = "test"
-        val propertyValue = "bla"
+        val propertyValue = "files"
         val properties = arrayListOf<Property>(Property(0, propertyName, propertyValue))
         val tableObject = runBlocking { TableObject.create(uuid, tableId, properties) }
 
@@ -116,10 +119,134 @@ class TableObjectInstrumentedTest {
         val tableObject = runBlocking { TableObject.create(1) }
 
         // Act
-        val propertyValue = tableObject.getPropertyValue("bla")
+        val propertyValue = tableObject.getPropertyValue("files")
 
         // Assert
         Assert.assertNull(propertyValue)
     }
     // End getPropertyValue tests
+
+    // delete tests
+    @Test
+    fun deleteShouldSetTheUploadStatusOfTheTableObjectToDeletedWhenTheUserIsLoggedIn(){
+        // Arrange
+        ProjectInterface.localDataSettings?.setStringValue(Dav.jwtKey, Constants.jwt)
+        val tableId = 2
+        val uuid = UUID.randomUUID()
+        val properties = arrayListOf<Property>(
+                Property(0, "test1", "blabla"),
+                Property(0, "test2", "blablabla"))
+        val tableObject = runBlocking {
+            TableObject.create(uuid, tableId, properties)
+        }
+
+        // Act
+        runBlocking { tableObject.delete() }
+
+        // Assert
+        val tableObject2 = runBlocking { Dav.Database.getTableObject(uuid) }
+        Assert.assertNotNull(tableObject2)
+        Assert.assertEquals(TableObjectUploadStatus.Deleted, tableObject2!!.uploadStatus)
+    }
+
+    @Test
+    fun deleteShouldDeleteTheFileOfATableObjectAndSetTheUploadStatusToDeletedWhenTheUserIsLoggedIn(){
+        // Arrange
+        ProjectInterface.localDataSettings?.setStringValue(Dav.jwtKey, Constants.jwt)
+        val tableId = 2
+        val uuid = UUID.randomUUID()
+
+        // Copy the file into the cache
+        val assetManager = InstrumentationRegistry.getContext().assets
+        val inputStream = assetManager.open("files/test/icon.ico")
+        val file = File.createTempFile("icon", ".ico")
+        file.copyInputStreamToFile(inputStream)
+        inputStream.close()
+
+        val tableObject = runBlocking {
+            TableObject.create(uuid, tableId, file)
+        }
+
+        Assert.assertTrue(tableObject.file?.exists() ?: false)
+
+        // Act
+        runBlocking { tableObject.delete() }
+
+        // Assert
+        Assert.assertFalse(tableObject.file?.exists() ?: true)
+        Assert.assertEquals(TableObjectUploadStatus.Deleted, tableObject.uploadStatus)
+
+        val tableObject2 = runBlocking { Dav.Database.getTableObject(uuid) }
+        Assert.assertNotNull(tableObject2)
+        Assert.assertEquals(TableObjectUploadStatus.Deleted, tableObject2?.uploadStatus)
+    }
+
+    @Test
+    fun deleteShouldDeleteTheTableObjectImmediatelyWhenTheUserIsNotLoggedIn(){
+        // Arrange
+        ProjectInterface.localDataSettings?.setStringValue(Dav.jwtKey, "")
+        val tableId = 3
+        val uuid = UUID.randomUUID()
+        val properties = arrayListOf<Property>(
+                Property(0, "page1", "Hello World"),
+                Property(0, "page2", "Hallo Welt"))
+        val tableObject = runBlocking {
+            TableObject.create(uuid, tableId, properties)
+        }
+
+        val firstPropertyId = tableObject.properties[0].id
+        val secondPropertyId = tableObject.properties[1].id
+
+        // Act
+        runBlocking { tableObject.delete() }
+
+        // Assert
+        val tableObjectFromDatabase = runBlocking { Dav.Database.getTableObject(uuid) }
+        Assert.assertNull(tableObjectFromDatabase)
+
+        val firstPropertyFromDatabase = database.propertyDao().getProperty(firstPropertyId)
+        Assert.assertNull(firstPropertyFromDatabase)
+
+        val secondPropertyFromDatabase = database.propertyDao().getProperty(secondPropertyId)
+        Assert.assertNull(secondPropertyFromDatabase)
+    }
+
+    @Test
+    fun deleteShouldDeleteTheFileOfTheTableObjectAndDeleteTheTableObjectImmediatelyWhenTheUserIsNotLoggedIn(){
+        // Arrange
+        ProjectInterface.localDataSettings?.setStringValue(Dav.jwtKey, "")
+        val tableId = 3
+        val uuid = UUID.randomUUID()
+
+        // Copy the file into the cache
+        val assetManager = InstrumentationRegistry.getContext().assets
+        val inputStream = assetManager.open("files/test/icon.ico")
+        val file = File.createTempFile("icon", ".ico")
+        file.copyInputStreamToFile(inputStream)
+        inputStream.close()
+
+        val tableObject = runBlocking {
+            TableObject.create(uuid, tableId, file)
+        }
+
+        Assert.assertTrue(tableObject.file?.exists() ?: false)
+
+        // Act
+        runBlocking { tableObject.delete() }
+
+        // Assert
+        Assert.assertFalse(tableObject.file?.exists() ?: false)
+        val tableObjectFromDatabase = runBlocking { Dav.Database.getTableObject(uuid) }
+        Assert.assertNull(tableObjectFromDatabase)
+    }
+    // End delete tests
+
+    private fun File.copyInputStreamToFile(inputStream: InputStream) {
+        // Return the progress as int between 0 and 100
+        inputStream.use { input ->
+            this.outputStream().use { fileOut ->
+                input.copyTo(fileOut)
+            }
+        }
+    }
 }
