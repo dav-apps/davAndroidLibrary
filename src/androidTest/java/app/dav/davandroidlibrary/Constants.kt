fb904388adcbe2969547b1eac8ec6895227c782c
David <dav2070@web.de>
2018-12-30 23:07:01 +0100

    Add data of the test table objects

--- src/androidTest/java/app/dav/davandroidlibrary/Constants.kt
+++ src/androidTest/java/app/dav/davandroidlibrary/Constants.kt
@@ -1,6 +1,9 @@
 package app.dav.davandroidlibrary
 
 import app.dav.davandroidlibrary.models.DavPlan
+import app.dav.davandroidlibrary.models.Property
+import app.dav.davandroidlibrary.models.TableObject
+import java.util.*
 
 object Constants {
     const val apiKey = "MhKSDyedSw8WXfLk2hkXzmElsiVStD7C8JU3KNGp"
@@ -13,4 +16,19 @@ object Constants {
     const val testuserEmail = "davclasslibrarytest@dav-apps.tech"
     const val testuserUsername = "davClassLibraryTestUser"
     val testuserPlan = DavPlan.Free
+
+    const val testDataFirstPropertyName = "page1"
+    const val testDataSecondPropertyName = "page2"
+    const val testDataFirstTableObjectFirstPropertyValue = "Hello World"
+    const val testDataFirstTableObjectSecondPropertyValue = "Hallo Welt"
+    const val testDataSecondTableObjectFirstPropertyValue = "Table"
+    const val testDataSecondTableObjectSecondPropertyValue = "Tabelle"
+    val testDataFirstTableObject = TableObject(UUID.fromString("642e6407-f357-4e03-b9c2-82f754931161"), testDataTableId, arrayListOf(
+        Property(0, testDataFirstPropertyName, testDataFirstTableObjectFirstPropertyValue),
+        Property(0, testDataSecondPropertyName, testDataFirstTableObjectSecondPropertyValue)
+    ))
+    val testDataSecondTableObject  = TableObject(UUID.fromString("8d29f002-9511-407b-8289-5ebdcb5a5559"), testDataTableId, arrayListOf(
+        Property(0, testDataFirstPropertyName, testDataSecondTableObjectFirstPropertyValue),
+        Property(0, testDataSecondPropertyName, testDataSecondTableObjectSecondPropertyValue)
+    ))
 }
